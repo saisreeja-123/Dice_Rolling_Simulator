90407eada3a2a7b240cff9ad2db54a20c48fe571
saisreeja-123 <saisreejanaraboina126@gmail.com>
2026-07-27 08:18:31 +0530

    Add README for Dice Rolling Simulator project
    Added project details including features and concepts used.

---
+++ README.d
@@ -0,0 +1,14 @@
+Project: Dice Rolling Simulator
+Language: Java
+Features:
+Roll one or more dice
+Random dice values (1–6)
+ASCII dice faces
+Input validation
+Concepts Used:
+Scanner
+Random
+Loops
+Methods
+Switch
+Exception handling
